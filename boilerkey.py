7fc5e2069bfdf8b31819b2cfe009214bd71e3011
Jeremy Gleeson <jeremywgleeson@gmail.com>
2020-07-30 01:13:34 -0700

    Added support for generating and storing authentication cookies.

--- boilerkey.py
+++ boilerkey.py
@@ -2,6 +2,7 @@ import base64
 import json
 import os
 import sys
+import pickle # json serialization loses data for cookie jars
 
 try:
     import requests
@@ -11,8 +12,9 @@ except ImportError:
     print("Please run 'pip3 install pyotp requests'")
     sys.exit(1)
 
-CONFIG_PATH = os.path.dirname(os.path.realpath(__file__)) + "/config.json"
-COUNTER_PATH = os.path.dirname(os.path.realpath(__file__)) + "/counter.json"
+CONFIG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.json")
+COUNTER_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "counter.json")
+COOKIE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "cookies.p")
 
 # DO NOT LOSE YOUR WAAAAAAY!
 
@@ -90,6 +92,20 @@ def getCounter():
         return json.load(f)["counter"]
 
 
+def setCookies(cookieDict):
+    with open(COOKIE_PATH, "wb") as f:
+        pickle.dump(cookieDict, f)
+
+
+def getCookies():
+    # returns cookiejar object
+    if os.path.isfile(COOKIE_PATH):
+        with open(COOKIE_PATH, "rb") as f:
+            return pickle.load(f)
+    else:
+        return None
+
+
 def generatePassword():
     config = getConfig()
     counter = getCounter()
@@ -107,6 +123,73 @@ def generatePassword():
 
     return password
 
+def getLtSecret(html):
+    # length of secret is always 41
+    ltLen = 41
+    preStr = '<input type="hidden" name="lt" value="'
+    matchIndex = html.find(preStr)
+    if matchIndex != -1:
+        if len(html) > matchIndex + ltLen:
+            # need to shift index back to include 'LT-'
+            startKeyIndex = matchIndex + len(preStr)
+            endKeyIndex = startKeyIndex + ltLen
+            return html[startKeyIndex:endKeyIndex]
+    return None
+
+def getExecutionSecret(html):
+    # length of secret is always 4
+    executionLen = 4
+    preStr = '<input type="hidden" name="execution" value="'
+    matchIndex = html.find(preStr)
+    if matchIndex != -1:
+        if len(html) > matchIndex + executionLen:
+            startKeyIndex = matchIndex + len(preStr)
+            endKeyIndex = startKeyIndex + executionLen
+            return html[startKeyIndex:endKeyIndex]
+    return None
+
+def generateAuthCookies(save=False):
+    # cookies do not expire, so you can save across runs
+
+    conf = getConfig()
+    if 'username' not in conf:
+        print("You need to configure your username to use generateAuthCookies")
+        return None
+
+    loginUrl = "https://www.purdue.edu/apps/account/cas/login"
+
+    payload = {
+        'MIME Type': 'application/x-www-form-urlencoded',
+        'username': conf["username"],
+        'password': generatePassword(),
+        # 'lt': None,
+        # 'execution': None,
+        '_eventId': 'submit',
+        'submit': 'Login'
+    }
+
+    with requests.Session() as sesh:
+        initGet = sesh.get(loginUrl)
+
+        ltSecret = getLtSecret(initGet.text)
+        executionSecret = getExecutionSecret(initGet.text)
+
+        if not ltSecret or not executionSecret:
+            print("Error: Secrets could not be parsed")
+            return None
+        else:
+            payload['lt'] = ltSecret
+            payload['execution'] = executionSecret
+            loginPost = sesh.post(loginUrl, data=payload)
+
+            # post-auth response from loginUrl will always contain this:
+            if "Central Authentication Service" in loginPost.text:
+                if save:
+                    setCookies(sesh.cookies)
+                return sesh.cookies
+            else:
+                print("Error: Check credentials or try again")
+    return None
 
 def askForInfo():
     print(
@@ -136,8 +219,17 @@ def askForInfo():
         pin = ""
         print("Invalid pin")
 
+    print(
+        """5. (Optional) In order to generate authentication cookies,
+    script needs your username (XXXXXXX@purdue.edu). You can leave this empty."""
+    )
+
+    username = input()
+
     activationData = getActivationData(activationCode)
     activationData["pin"] = pin
+    if username:
+        activationData["username"] = username
     createConfig(activationData)
     setCounter(0)
     print("Setup successful!")

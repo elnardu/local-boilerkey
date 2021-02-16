9a404c07c7cd0dc9e407a9ba3ce674d3236db353
Jeremy Gleeson <jeremywgleeson@gmail.com>
2021-02-16 12:26:46 -0600

    Added ability to generate boilerkey device config

--- boilerkey.py
+++ boilerkey.py
@@ -2,6 +2,9 @@ import base64
 import json
 import os
 import sys
+import random
+import re
+from string import ascii_lowercase
 
 try:
     import requests
@@ -64,9 +67,9 @@ def validateLink(link):
         assert "m-1b9bef70.duosecurity.com" in link
         code = link.split("/")[-1]
         assert len(code) == 20
-        return True, code
+        return code
     except Exception:
-        return False, None
+        return None
 
 
 def createConfig(activationData):
@@ -107,6 +110,134 @@ def generatePassword():
 
     return password
 
+def addBoilerKey(username, password, name="local_boilerkey"):
+    """
+    Send requests to purdue boilerkey management to add new boilerkey device
+
+    Returns 20 digit code used for duomobile registration
+
+    :param username: :class:`str` instance purdue username
+    :param password: :class:`str` instance password correlating
+        with username OTP: (****,******) OR 2fa password (****,push*)
+    :param password: :class:`str` instance name to register new boilerkey as
+        (Duplicate will have random string appended)
+    :raises ValueError: for invalid credentials (username, password)
+    :raises ValueError: for invalid name
+    :rtype: str OR None
+    """
+
+    headers = {
+    "User-Agent":
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36",
+    }
+
+    name_re = re.compile("^[a-zA-Z0-9_]{1,32}$")
+    if not name_re.match(name):
+        raise ValueError(f"Invalid name: '{name}'\nMust be:\n"
+                         "\t-Under 32 characters\n"
+                         "\t-Contain only a-Z 0-9 and '_'")
+    with requests.Session() as s:
+        s.headers.update(headers)
+        main_get = s.get("https://www.purdue.edu/apps/account/BoilerKeySelfServe")
+
+        # find lt secret
+        text = main_get.text
+        sub_to_find = '<input type="hidden" name="lt" value="'
+        start_ind = text.find(sub_to_find) + len(sub_to_find)
+        lt = text[start_ind:text.find('"', start_ind)]
+
+        # find post url
+        sub_to_find = '<form id="fm1" action="'
+        start_ind = text.find(sub_to_find) + len(sub_to_find)
+        login_form_url = "https://www.purdue.edu" + text[start_ind:text.find('"', start_ind)]
+
+        login_payload = {
+            'username': username,
+            'password': password,
+            'lt': lt,
+            'execution': 'e1s1',
+            '_eventId': 'submit',
+            'submit': 'Login'
+        }
+        login_form_post = s.post(login_form_url, data=login_payload)
+
+        # check for auth failure
+        if "https://www.purdue.edu/apps/account/flows/BoilerKey" not in login_form_post.url and "https://www.purdue.edu/apps/account/cas/login" in login_form_post.url:
+            raise ValueError("Invalid credentials")
+        else:
+            print("Authenticated successfully\nPlease allow ~1min for slow purdue sites")
+
+        new_post_data = {
+            "_eventId": "boilerKeyDuoMobileCreate",
+            "_flowExecutionKey": "e1s1",
+            "phoneName":None,
+        }
+        new_post = s.post("https://www.purdue.edu/apps/account/flows/BoilerKey?execution=e1s1", data=new_post_data)
+
+        # get flow string number (required for request to be processed)
+        text = new_post.url
+        start_ind = text.find("e1s") + 3
+        flow_str = "e1s" + text[start_ind:]
+
+        cont_post_data = {
+            "_eventId": "duoMobileCreateProcessDownloadAppAction",
+            "_flowExecutionKey": flow_str
+        }
+        cont_post = s.post("https://www.purdue.edu/apps/account/flows/BoilerKey?execution={}".format(flow_str), data=cont_post_data)
+
+        # get flow string number (required for request to be processed)
+        text = cont_post.url
+        start_ind = text.find("e1s") + 3
+        flow_str = "e1s" + text[start_ind:]
+
+        pin_post_data = {
+            "_eventId": "duoMobileCreateProcessSetPinAction",
+            "_flowExecutionKey": flow_str,
+            "existingPin": password.split(",")[0]
+        }
+        pin_post = s.post("https://www.purdue.edu/apps/account/flows/BoilerKey?execution={}".format(flow_str), data=pin_post_data)
+
+        # get flow string number (required for request to be processed)
+        text = pin_post.url
+        start_ind = text.find("e1s") + 3
+        flow_str = "e1s" + text[start_ind:]
+
+        name_post_data = {
+            "_eventId": "duoMobileCreateProcessNameDeviceAction",
+            "_flowExecutionKey": flow_str,
+            "phoneName": name
+        }
+
+        name_post = s.post("https://www.purdue.edu/apps/account/flows/BoilerKey?execution={}".format(flow_str), data=name_post_data)
+
+        for i in range(0,5):
+            if "Sorry, you already have a Duo Mobile BoilerKey with that name" not in name_post.text:
+                break
+
+            # get flow string number (required for request to be processed)
+            text = name_post.url
+            start_ind = text.find("e1s") + 3
+            flow_str = "e1s" + text[start_ind:]
+
+            new_name = name + "_" + ''.join(random.choice(ascii_lowercase) for i in range(4))
+            if not name_re.match(new_name):
+                raise ValueError(f"Invalid name: '{name}'\nMust be:\n"
+                                 "\t-Under 32 characters\n"
+                                 "\t-Contain only a-Z 0-9 and '_'")
+            name_post_data = {
+                "_eventId": "duoMobileCreateProcessNameDeviceAction",
+                "_flowExecutionKey": flow_str,
+                "phoneName": new_name
+            }
+
+            name_post = s.post("https://www.purdue.edu/apps/account/flows/BoilerKey?execution={}".format(flow_str), data=name_post_data)
+
+        text = name_post.text
+        sub_to_find = 'https://m-1b9bef70.duosecurity.com/activate/'
+        start_ind = text.find(sub_to_find)
+        link = text[start_ind:text.find('"', start_ind)]
+
+        return validateLink(link)
 
 def askForInfo():
     print(
@@ -118,12 +249,12 @@ def askForInfo():
    under the qr code right here and press Enter"""
     )
 
-    valid = False
-    while not valid:
+    activationCode = None
+    while not activationCode:
         link = input()
-        valid, activationCode = validateLink(link)
+        activationCode = validateLink(link)
 
-        if not valid:
+        if not activationCode:
             print("Invalid link. Please try again")
 
     print(
@@ -144,11 +275,58 @@ def askForInfo():
 
     print("Here is your password: ", generatePassword())
 
+def getInput():
+    """
+    Get username and password from user
+
+    :rtype: tuple (username, password)
+    """
+    username = input("Purdue username\n:")
+    while not username:
+      username = input("\nYou must enter a username\nPurdue username\n:")
+
+    pass_re = re.compile("\d{4},(\d{6}|push\d*)")
+    password = input("Purdue password\n(Password may be of type '****,push', or '****,******')\n:")
+    re_match = pass_re.match(password)
+    while not re_match:
+        password = input("\nInvalid password\nPurdue password\n(Password may be of type '****,push', or '****,******')\n:")
+        re_match = pass_re.match(password)
+
+    return username, password
+
+
+def autoSetup():
+    """
+    Setup with automated boilerkey device creation
+
+    :rtype: None
+    """
+    print("This setup will add a new boilerkey device to your account.\n"
+          "Please note: you will receive an email notifying you your boilerkey settings have been changed\n"
+          "If you choose to use '****,push' type password, you will need to allow login on duo mobile\n"
+          "You may revoke access at any time from the boilerkey managment page\n\n"
+          "Setup:")
+    username, password = getInput()
+    link = None
+    while not link:
+        try:
+            link = addBoilerKey(username, password)
+        except ValueError as ve:
+            print('\n', ve)
+            username, password = getInput()
+
+    activationData = getActivationData(link)
+    activationData["pin"] = password.split(',')[0]
+    createConfig(activationData)
+    setCounter(0)
+    print("Setup successful!")
+
+    print("Here is your password: ", generatePassword())
 
 def main():
     if not os.path.isfile(CONFIG_PATH) or not os.path.isfile(COUNTER_PATH):
         print("Configuration files not found! Running setup...")
-        askForInfo()
+        autoSetup()
     else:
         print(generatePassword())
 
